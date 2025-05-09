476430c9e807e0c392f68a13edf2754366f0db5d
MARCELO ROSA VITOLA <0103793@acad.redes-ienh.com.br>
2025-05-09 10:55:18 -0300

    Sum list

--- my-java-project/src/main/java/App.java
+++ my-java-project/src/main/java/App.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //import our class function as transf
+        Arrayp transf = new Arrayp();
+        String numbers = "1 2 3 6 8 2 9";
+
+        //split
+        String[] numlist = numbers.split(" ");
+        int summed = 0;
+        for (String i : numlist) {
+            System.out.println(i);
+            summed += Integer.parseInt(i);
+        }
+        //we use transf.transform(numlist) to print the list using the class we did.
+        System.out.println("Content: " + transf.transform(numlist));
+        System.out.println("Result: " + summed); 
+    }
+}
+
+//This class is like a function to print lists
+class Arrayp {
+    //Say that the function needs a list
+    String[] list;
+
+    //Makes a function that transforms a list into a printable array
+    public String transform(String[] list) {
+        return Arrays.toString(list);
     }
 }
